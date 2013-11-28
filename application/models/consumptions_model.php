9ca1a07edc46233b192a12597c3a2cf16696a4f6
Thomb <thomb65@hotmail.com>
2013-11-28 14:25:24 +0100

    consumptions model afgerond: getConsumptions haalt alle consumptions op, getConsumption slechts 1 consumption middels een variabel.

--- application/models/consumptions_model.php
+++ application/models/consumptions_model.php
@@ -1,10 +1,28 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
+<?php if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
 
-<body>
-</body>
-</html>
+class consumptions_model extends CI_Model
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+	
+	function getConsumption($user_id)
+	{
+		$sql = "SELECT * FROM consumptions WHERE id = ? LIMIT 1";
+		
+		$result = $this->db->query($sql, $id);
+		$row = $result->row_array(); 
+		
+		return $row;
+	}
+	
+	function getAllConsumptions()
+	{
+		$sql = "SELECT * FROM consumptions";
+		
+		return $sql;
+	}
+}
+?>
