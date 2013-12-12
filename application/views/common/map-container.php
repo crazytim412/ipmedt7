bc5ec65a5c3aed5b83798b9938f0842f9776068d
Benno van Assen <vanassen.benno@gmail.com>
2013-12-12 13:46:02 +0100

    verse 1 bar.php, location_model.php, map-container.php, disco.php

---
+++ application/views/common/map-container.php
@@ -0,0 +1,26 @@
+<div id="map-container">
+			<div id="header">
+				<h1 class="logo"><img src="img/logo.png" /></h1>
+				<ul id="header_stats">
+					<li>
+						<label>Humeur</label>
+						<span>100</span>
+					</li>
+					<li>
+						<label>Energie</label>
+						<span>60</span>
+					</li>
+					<li>
+						<label>Dag</label>
+						<span>1</span>
+					</li>
+					<li>
+						<label>Score</label>
+						<span>4677</span>
+					</li>
+				</ul>
+
+				<div id="optionsBut"></div>
+				<div class="clear"></div>
+				<div id="energymeter"></div>
+			</div>
---
+++ application/views/disco.php
@@ -0,0 +1,9 @@
+<?php $this->load->view("common/header");?>
+
+
+
+
+<?php $this->load->view("common/map-container");?>
+
+
+<?php $this->load->view("common/footer");?>
