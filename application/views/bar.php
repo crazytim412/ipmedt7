100f339ac07e9a5a26babc8aca7e12d8f33dc47f
Benno van Assen <vanassen.benno@gmail.com>
2013-12-16 15:45:54 +0100

    Eerste opzetten bar controller, location

--- application/views/bar.php
+++ application/views/bar.php
@@ -1,11 +1,15 @@
 <?php $this->load->view("common/header"); ?>
 
+<div id="barBackground"	>
+
 Welkom in de kroeg<br>
 U heeft nog <?php echo $this->session->userdata("consumptions_left"); ?> consumpties over.
 <br><br>
 
-<?php foreach($consumptions->result() as $row): ?>
-<a href="/index.php/location/consume/<?php echo $row->id; ?>/">Neem een <?php echo $row->name; ?> (<?php echo $row->consumption_weight; ?>) </a><br>
-<?php endforeach; ?><br><br>
+
 
 <a href="/index.php/location/exitlocation">Verlaat de kroeg</a>
+
+</div>
+
+<?php $this->load->view("common/footer");?>
