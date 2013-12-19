a9bb9e93e6545c02b4328dec09df9645120bb992
duncan pronk <ct412@live.nl>
2013-12-19 10:28:36 +0100

    Oopsie, verkeerde link ingevoegd

--- application/controllers/register.php
+++ application/controllers/register.php
@@ -34,7 +34,7 @@ class Register extends CI_Controller {
 				{
 					$data["error"]="Gefeliciteerd, je bent geregistreerd.";
 					sleep(10);
-					redirect("/","refresh");
+					redirect("/index.php/avatar","refresh");
 				}
 			}
 			else
