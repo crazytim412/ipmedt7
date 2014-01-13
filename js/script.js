bc7c59f995a54cab488a336f53dea85aeac95eb6
Wessel <admin@wesselstam.nl>
2014-01-13 11:21:11 +0100

    De jQuery „Done” methode stopte spontaan met werken waardoor de hoeveelheid overgebleven consumpties niet meer klopt. Dmv de methode Success is dit probleem opgelost.

--- js/script.js
+++ js/script.js
@@ -24,18 +24,19 @@ $(document).ready(function(){
 	
 	$(".consumptions .consumption").click(function(){
 		$.ajax({
-			url: "/index.php/location/consume/"+$(this).attr("data-id")+"/"
-		}).done(function( data ) {
-			if ( console && console.log ) {
-				if(data == "-1")
-				{
-					$('.error_message').text("U heeft geen consumpties meer over, keer terug naar de stad.");
-				}
-				else
-				{
-					$('.consumptions_left').text(data);
-					console.log(data);
-				}
+			url: "/index.php/location/consume/"+$(this).attr("data-id")+"/",
+			success: function(data){
+				if ( console && console.log ) {
+					if(data == "-1")
+					{
+						$('.error_message').text("U heeft geen consumpties meer over, keer terug naar de stad.");
+					}
+					else
+					{
+						$('.consumptions_left').text(data);
+						console.log(data);
+					}
+				}	
 			}
 		});
 	});
